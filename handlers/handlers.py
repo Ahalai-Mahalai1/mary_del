b4d9c7b77233006d30efe76ef03b2c181bc25a60
Andrey <alpha2010@mail.ru>
2025-07-31 14:09:11 +0300

    new one

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,6 +1,6 @@
 import asyncio
 from random import randint
-
+import os
 from config.config import Config, load_config
 from aiogram import Bot, Router, F
 from aiogram.types import Message
@@ -52,11 +52,18 @@ async def process_help_command(message: Message, state: FSMContext):
 @router.message(StateFilter(FSMFillForm.wait_text), F.document)
 async def process_name_sent(message: Message, state: FSMContext):
     global lst
-    f = open (message.document.file_name, 'r')
-    text = f.read()
+    document = message.document
+    file = await bot.get_file(document.file_id)
+    file_path = file.file_path
+    local_path = f"./{document.file_name}"
+    await bot.download_file(file_path, destination=local_path)
+    with open(local_path, "r", encoding="utf-8") as f:
+        text = f.read()
     lst=text.split('\n')
+    if os.path.exists(local_path):
+          os.remove(local_path)
     await message.answer(text='Спасибо!\n\nА теперь введите через сколько должны удаляться пользователи в секундах')
-    # Устанавливаем состояние ожидания ввода возраста
+    # # Устанавливаем состояние ожидания ввода возраста
     await state.set_state(FSMFillForm.wait_time)
 @router.message(StateFilter(FSMFillForm.wait_text))
 async def warning_not_text(message: Message):
@@ -70,11 +77,11 @@ async def process_time_sent(message: Message, state: FSMContext):
     await message.answer(text='Спасибо!\n\nНачинаем удалять!')
     await asyncio.sleep(int(message.text))
     while lst:
-        pos=randint(0,len(lst))
+        pos=randint(0,len(lst)-1)
         try:
-            await bot.ban_chat_member(chat_id="@thisisachanel080725", user_id=lst[pos])
-            await asyncio.sleep(1)  # можно сразу разбанить, чтобы можно было снова вступить
-            await bot.unban_chat_member(chat_id="@thisisachanel080725", user_id=lst[pos])
+            #await bot.ban_chat_member(chat_id="@thisisachanel080725", user_id=lst[pos])
+            #await asyncio.sleep(1)  # можно сразу разбанить, чтобы можно было снова вступить
+            #await bot.unban_chat_member(chat_id="@thisisachanel080725", user_id=lst[pos])
             await bot.send_message(chat_id=message.chat.id, text=f"Пользователь {lst[pos]} был удалён.")
         except Exception as e:
             await bot.send_message(chat_id=message.chat.id, text=f"Ошибка при удалении: {e}")
